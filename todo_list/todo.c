d947666b2fc340e3a7fd0350cc0d1b75dc952be5
yorkings <yorkingsnyakundi2700@gmail.com>
2026-02-01 13:48:50 +0300

    todo list

---
+++ todo_list/todo.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_TASKS 100
+#define MAX_LENGTH 50
+
+void add_task(char tasks[][MAX_LENGTH], int *task_count) {
+    if (*task_count < MAX_TASKS) {
+        printf("Enter task: ");
+        fgets(tasks[*task_count], MAX_LENGTH, stdin);
+        tasks[*task_count][strcspn(tasks[*task_count], "\n")] = 0; // remove newline
+        (*task_count)++;
+        printf("Task added!\n");
+    } else {
+        printf("Task list full!\n");
+    }
+}
+
+void view_tasks(char tasks[][MAX_LENGTH], int task_count) {
+    if (task_count == 0) {
+        printf("No tasks yet.\n");
+    } else {
+        printf("\n--- Your Tasks ---\n");
+        for (int i = 0; i < task_count; i++) {
+            printf("%d. %s\n", i + 1, tasks[i]);
+        }
+    }
+}
+
+void delete_task(char tasks[][MAX_LENGTH], int *task_count) {
+    if (*task_count == 0) {
+        printf("No tasks to delete.\n");
+        return;
+    }
+
+    int del;
+    printf("Enter task number to delete: ");
+    scanf("%d", &del);
+    while (getchar() != '\n'); // clear buffer
+
+    if (del < 1 || del > *task_count) {
+        printf("Invalid task number.\n");
+    } else {
+        for (int i = del - 1; i < *task_count - 1; i++) {
+            strcpy(tasks[i], tasks[i + 1]);
+        }
+        (*task_count)--;
+        printf("Task deleted.\n");
+    }
+}
+
+int main() {
+    char tasks[MAX_TASKS][MAX_LENGTH];
+    int task_count = 0;
+    int choice;
+
+    do {
+        printf("\n--- To-Do List Menu ---\n");
+        printf("1. Add Task\n2. View Tasks\n3. Delete Task\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        while (getchar() != '\n'); // clear buffer
+
+        switch(choice) {
+            case 1:
+                add_task(tasks, &task_count);
+                break;
+            case 2:
+                view_tasks(tasks, task_count);
+                break;
+            case 3:
+                delete_task(tasks, &task_count);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice!\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
